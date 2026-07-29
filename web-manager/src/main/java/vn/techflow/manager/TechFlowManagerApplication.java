1bde1e77a0d3d01ad649ba8cd32ba5d9f254f91d
MAI QUOC HIEP <QuocHiep31003hiepmqss170645@fpt.edu.vn>
2026-07-29 21:48:26 +0700

    fix: enable VIA_DTO page serialization mode to suppress Spring Data warning

--- web-manager/src/main/java/vn/techflow/manager/TechFlowManagerApplication.java
+++ web-manager/src/main/java/vn/techflow/manager/TechFlowManagerApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import vn.techflow.manager.youtube.YouTubeConfiguration;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableConfigurationProperties(YouTubeConfiguration.class)
 public class TechFlowManagerApplication {
     public static void main(String[] args) {
